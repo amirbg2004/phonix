77be298917e6430ccc2ac1341453e1bd9a55ccbf
AymanAD <aymandandan24680@gmail.com>
2024-08-10 01:12:13 +0300

    account btn fixed

--- sidebar/sidebar.js
+++ sidebar/sidebar.js
@@ -1,8 +1,18 @@
-const sidebarButtons = document.querySelectorAll(".sub-option a, .option:not(title) a");
+const sidebarButtons = document.querySelectorAll(".option:not(title) a");
 
 sidebarButtons.forEach((btn) =>
   btn.addEventListener("click", () => {
     window.location.href = `../results-page/results.html?collection=${btn.dataset.filter}`;
-    console.log("Hello");
   })
 );
+
+function checkAccountInUse() {
+  let accSidebarBtn =  document.querySelector('.option.title:nth-last-of-type(1) a');
+  let account = JSON.parse(localStorage.getItem("account-in-use"));
+  if(account){
+    accSidebarBtn.href = "../cart-page/cart.html";
+  }else{
+    accSidebarBtn.href = "../login-page/login.html";
+  }
+}
+checkAccountInUse();
